c27786e4727b30a2249f7a6b117fda902a9e48f3
eydan1601cr <eydan1601@gmail.com>
2026-09-06 11:47:04 -0600

    Diseño principal

--- app/src/main/java/com/example/notas_app_sqlite/MainActivity.kt
+++ app/src/main/java/com/example/notas_app_sqlite/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.notas_app_sqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notas_app_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.FEVAagregarNota.setOnClickListener {
+            Toast.makeText(applicationContext,
+                "Has presionado en el Fav",
+                Toast.LENGTH_SHORT).show()
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
